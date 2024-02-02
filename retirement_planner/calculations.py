8074a00f36feee2ec61ea0aa6babd54d8e3d2057
Tim McFadden <mtik00@users.noreply.github.com>
2024-02-02 07:29:50 -0700

    feat: decent start to calculation

--- retirement_planner/calculations.py
+++ retirement_planner/calculations.py
@@ -16,20 +16,21 @@ INFLATION = {
 
 FIXED_INCOME = {
     1: 0,
-    55: 10000,  # NTSOC
-    59: 20000,  # PERA
-    67: 60000,  # PERA + SS
+    55: 10000,  # Spouse will continue to work
+    59: 20000,  # Pension
+    67: 50000,  # Pension plus Social Security
 }
 
 ANNUAL_YIELD = {
-    55: 0.08,
-    65: 0.08,
-    75: 0.06,
+    55: 0.07,
+    65: 0.06,
+    75: 0.05,
     85: 0.03,
 }
 
 
-def get_closest(value, lookup: dict):
+def get_previous(value, lookup: dict):
+    """Search a dictionary for a value < the key."""
     keys = sorted(lookup.keys())
 
     for index, key in enumerate(keys[1:]):
@@ -39,12 +40,19 @@ def get_closest(value, lookup: dict):
         return lookup[keys[-1]]
 
 
-def create_table(
-    starting_balance: int, starting_age: int, starting_distribution, number_of_years=100
+def calculate_scenario(
+    starting_balance: int,
+    starting_age: int,
+    starting_distribution,
+    max_number_of_years=100,
 ):
     """
     The annual calculation is:
-        start + (distribution * inflation) - distributions + yield + fixed income.
+        start - (distribution * inflation) - distributions + yield + fixed income.
+
+    The numbers are basic annual calculations with the most pessimistic view.  All
+    deductions are taken at the start of the year, earnings are then calculated from
+    that amount, then fixed income is added to the total.
     """
     total_balance = starting_balance
     distributions = starting_distribution
@@ -56,20 +64,27 @@ def create_table(
             "starting balance",
             "fixed income",
             "withdrawal",
-            "yield",
+            "earnings",
             "ending balance",
         )
     )
 
-    for age in range(starting_age, starting_age + number_of_years):
+    for age in range(starting_age, starting_age + max_number_of_years):
         starting_balance = total_balance
-        inflation = get_closest(age, INFLATION)
-        fixed_income = get_closest(age, FIXED_INCOME)
-        yield_rate = get_closest(age, ANNUAL_YIELD)
-        yield_ = yield_rate * (total_balance - (distributions - fixed_income))
+        inflation = get_previous(age, INFLATION)
+        fixed_income = get_previous(age, FIXED_INCOME)
+        yield_rate = get_previous(age, ANNUAL_YIELD)
+
+        # Increase annual distribution amount based on inflation
         distributions = distributions * (1 + inflation)
 
-        total_balance = total_balance - distributions + fixed_income + yield_
+        # Calculate the yield as if all distributions were taken on day 1 and
+        # before fixed income (most pessimistic view).
+        left_overs = starting_balance - distributions
+        earnings = yield_rate * left_overs
+
+        # Add everything together
+        total_balance = left_overs + fixed_income + earnings
 
         print(
             row.format(
@@ -77,7 +92,7 @@ def create_table(
                 locale.currency(starting_balance, grouping=True),
                 locale.currency(fixed_income, grouping=True),
                 locale.currency(distributions, grouping=True),
-                locale.currency(yield_, grouping=True),
+                locale.currency(earnings, grouping=True),
                 locale.currency(total_balance, grouping=True),
             )
         )
@@ -88,7 +103,12 @@ def create_table(
 
 
 def main():
-    create_table(2_500_000, 52, 100_000, number_of_years=100)
+    calculate_scenario(
+        starting_balance=1_000_000,
+        starting_age=60,
+        starting_distribution=100_000,
+        max_number_of_years=100,
+    )
 
 
 if __name__ == "__main__":
